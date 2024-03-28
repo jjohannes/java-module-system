c07c8e1ef0a4f0bf599d8f8b91a69ed772551885
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-03-28 14:28:18 +0100

    Patch Guava metadata

--- gradle/plugins/src/main/kotlin/metadata-patch.gradle.kts
+++ gradle/plugins/src/main/kotlin/metadata-patch.gradle.kts
@@ -2,3 +2,20 @@ plugins {
     id("org.gradlex.jvm-dependency-conflict-resolution")
     id("org.gradlex.extra-java-module-info")
 }
+
+// Patch the Maven/Gradle metadata (*.pom and *.module files)
+jvmDependencyConflicts {
+    patch {
+        module("com.google.guava:guava") {
+            removeDependency("com.google.code.findbugs:jsr305")
+            removeDependency("org.checkerframework:checker-qual")
+            removeDependency("com.google.errorprone:error_prone_annotations")
+        }
+    }
+}
+
+// Patch the Java Module System metadata (module-info.class in Jar file)
+extraJavaModuleInfo {
+    module("com.google.guava:guava", "com.google.common")
+    module("com.google.guava:failureaccess", "com.google.common.util.concurrent.internal")
+}
