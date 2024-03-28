8ad29542f88716a82d76227de65512c09cb02043
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-03-28 11:50:26 +0100

    Use 'AmazingService' in 'app' module

--- modules/app/src/main/java/app/App.java
+++ modules/app/src/main/java/app/App.java
@@ -1,7 +1,10 @@
 package app;
 
+import amazing.AmazingService;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from module " + App.class.getModule().getName());
+        AmazingService.DEFAULT.compute();
     }
 }
