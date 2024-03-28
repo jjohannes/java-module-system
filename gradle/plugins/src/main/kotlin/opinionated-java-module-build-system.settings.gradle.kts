3a7823ef1c641b85100c431f65ab4c0619742870
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-03-28 13:27:33 +0100

    Let Gradle know that modules are Java modules

--- gradle/plugins/src/main/kotlin/opinionated-java-module-build-system.settings.gradle.kts
+++ gradle/plugins/src/main/kotlin/opinionated-java-module-build-system.settings.gradle.kts
@@ -5,3 +5,21 @@ layout.rootDirectory.dir("modules").asFile.listFiles()?.forEach { folder ->
     }
 }
 
+val mainClassName = "app.App"
+
+gradle.beforeProject {
+    when {
+        layout.projectDirectory.file("src/main/java/${mainClassName.replace('.', '/')}.java").asFile.exists() -> {
+            apply(plugin = "application")
+            apply(plugin = "java-module")
+            extensions.configure<JavaApplication> {
+                mainClass = mainClassName
+                mainModule = mainClassName.substring(0, mainClassName.lastIndexOf('.'))
+            }
+        }
+        else -> {
+            apply(plugin = "java-library")
+            apply(plugin = "java-module")
+        }
+    }
+}
