a196debff7e4322fca98e43236ef44f4f3e7f21d
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-03-28 13:32:37 +0100

    Examples of tweaking the Java building process

--- gradle/plugins/src/main/kotlin/java-module.gradle.kts
+++ gradle/plugins/src/main/kotlin/java-module.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     id("java")
 }
+
+// Tweak the Java building process (Java version, encoding, test memory settings, ...)
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+tasks.withType<Test>().configureEach {
+    maxParallelForks = 4
+    maxHeapSize = "1g"
+}
