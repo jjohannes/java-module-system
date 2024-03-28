f33246ae9db3ec2efccca2022d3ad141584890a9
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-03-28 14:25:59 +0100

    Use 'com.google.common' (Guava) module

--- gradle/versions/build.gradle.kts
+++ gradle/versions/build.gradle.kts
@@ -1,4 +1,5 @@
 moduleInfo {
+    version("com.google.common", "33.1.0-jre")
     version("org.apache.commons.text", "1.11.0")
     version("org.apache.commons.lang3", "3.14.0")
 }
--- modules/amazing/src/main/java/module-info.java
+++ modules/amazing/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module amazing {
+    requires com.google.common;
     requires org.apache.commons.text;
 
     exports amazing;
