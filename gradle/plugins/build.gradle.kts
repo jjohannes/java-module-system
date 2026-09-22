4bfefa2f1b6146ad79371e3af76b6ed405cb9dc1
Jendrik Johannes <jendrik@onepiece.software>
2026-09-22 10:20:19 +0200

    Update versions

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -7,12 +7,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:2.18.0")
+    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:3.19.2")
     implementation("com.gradle:develocity-gradle-plugin:4.0.2")
-    implementation("org.gradlex:extra-java-module-info:1.12")
-    implementation("org.gradlex:java-module-dependencies:1.9.1")
-    implementation("org.gradlex:java-module-packaging:1.0.1")
-    implementation("org.gradlex:java-module-testing:1.7")
-    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.4")
-    implementation("org.gradlex:reproducible-builds:1.0")
+    implementation("org.gradlex:extra-java-module-info:1.14.2")
+    implementation("org.gradlex:java-module-dependencies:1.13.2")
+    implementation("org.gradlex:java-module-packaging:1.3")
+    implementation("org.gradlex:java-module-testing:1.8.1")
+    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.5")
+    implementation("org.gradlex:reproducible-builds:1.1")
 }
